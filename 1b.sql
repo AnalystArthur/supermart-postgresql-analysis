2083c039d6ae19d4c81ae9744cd71740e1bb7dad
AnalystArthur <157991859+AnalystArthur@users.noreply.github.com>
2026-06-06 12:36:17 +0200

    Delete 1b.sql

--- 1b.sql
+++
@@ -1,7 +0,0 @@
---List the names of all distinct cities to which SuperMart has shipped at least one order. Sort 
---alphabetically. 
-
-SELECT DISTINCT shipping_city
-FROM orders
-ORDER BY shipping_city;
-
